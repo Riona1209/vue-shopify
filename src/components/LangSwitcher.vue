dc51dfc7bc937e641b9372fb72cc4ec639e920d8
Vegetable_builder <riona1209@outlook.com>
2018-03-05 07:32:28 -0500

    Remove bad paste

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -8,9 +8,6 @@
 <script>
 export default {
   name: 'LangSwitcher',
-  created() {
-    this.$store.dispatch('LOAD_CART');
-  },
   methods: {
     setLang(event) {
       this.$store.dispatch('SET_LANG', event.target.value);
